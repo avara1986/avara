e536ee16c71508ddaadbbe9dfd77a7354c7078dc
vagrant <vagrant@precise64.(none)>
2015-07-30 21:19:03 +0000

    Added Vagrant Files :muscle:

--- README.rst
+++ README.rst
@@ -6,3 +6,45 @@ Web de Alberto Vara
 Proyecto con Django y Google App Engine.
 
 Puedes contactar conmigo en a.vara.1986@gmail.com
+
+
+Installation
+============
+
+    sudo apt-get install dpkg-dev virtualbox-dkms
+
+    wget http://files.vagrantup.com/packages/0219bb87725aac28a97c0e924c310cc97831fd9d/vagrant_1.2.4_i686.deb
+
+    agrant box add hashicorp/precise64
+
+    vagrant init
+
+    git clone https://github.com/avara1986/avara .s
+
+    vagrant up
+
+## Setting up the virtual machine development environment
+
+SSH into the virtual machine:
+
+	vagrant ssh
+
+The source code is shared with the virtual machine in this directory:
+
+	cd avara
+
+Always run the following command to use the proper python virtual environment:
+
+	workon site
+
+Install the required python packages:
+
+	./install_deps.py (this will pip install requirements, and download the App Engine SDK)
+
+You can now start the development web server:
+
+	./manage.py runserver 192.168.100.3:8000
+
+Access this URL:
+
+* [http://192.168.100.3:8000](http://192.168.100.3:8000)
---
+++ bootstrap.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+if ! [ -L /home/vagrant/avara ]; then
+  rm -rf /home/vagrant/avara
+  ln -fs /vagrant /home/vagrant/avara
+fi
+PROJECT_DIR=/home/vagrant/avara
+USER_HOME=/home/vagrant
+
+echo "Environment installation is beginning. This may take a few minutes.."
+
+##
+#	Install core components
+##
+
+echo "Updating package repositories.."
+apt-get update
+
+echo "Installing required packages.."
+apt-get -y install git
+
+echo "Installing and upgrading pip.."
+apt-get -y install python-setuptools
+easy_install -U pip
+
+echo "Installing required packages for NFS file sharing for vagrant.."
+apt-get -y install nfs-common
+
+echo "Installing required packages for postgres.."
+apt-get -y install postgresql
+
+echo "Installing required packages for python package 'psycopg2'.."
+apt-get -y install python-dev python3-dev libpq-dev
+
+echo "Installing virtualenvwrapper from pip.."
+pip install virtualenvwrapper
+
+##
+#	Setup the database
+##
+
+echo "Configuring postgres.."
+sudo -u postgres psql -c "create user vagrant with password 'vagrant';"
+sudo -u postgres psql -c "create database vagrant;"
+sudo -u postgres psql -c "grant all privileges on database vagrant to vagrant;"
+
+##
+#	Setup virtualenvwrapper
+##
+echo "source /usr/local/bin/virtualenvwrapper.sh" >> ${USER_HOME}/.bashrc
+
+##
+#	Setup virtualenv
+##
+echo "Install the virtual environment.."
+sudo su - vagrant /bin/bash -c "source /usr/local/bin/virtualenvwrapper.sh;cd ${PROJECT_DIR};mkvirtualenv --python=`which python2.7` site; deactivate;"
+
+##
+#	Setup is complete.
+##
+echo ""
+echo "The environment has been installed."
+echo ""
+echo "You can start the machine by running: vagrant up"
+echo "You can ssh to the machine by running: vagrant ssh"
+echo "You can stop the machine by running: vagrant halt"
+echo "You can delete the machine by running: vagrant destroy"
+echo ""
+exit 0
